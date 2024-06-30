303e70d42164313941b2a49fc2158d7f70986c0d
Vinayakv01 <vinayakv949@gmail.com>
2024-06-30 13:05:29 +0530

    configuration

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import legacy from 'vite-plugin-legacy';
-import { createProxyMiddleware } from 'http-proxy-middleware';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'not IE 11'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-    }),
-  ],
+  plugins: [react()],
   server: {
-    middleware: [
-      createProxyMiddleware('/api', {
+    proxy: {
+      '/api': {
         target: 'http://apiweb.urbandiners.com',
         changeOrigin: true,
         secure: false,
-      }),
-    ],
+      },
+    },
   },
 });
